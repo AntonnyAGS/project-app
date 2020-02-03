e62320ca0b75a70e47a2f11f817c99ac9679856f
AntonnyAGS <antonny.santos@etec.sp.gov.br>
2020-02-02 21:52:33 -0300

    Conectando API

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div id="app">
-      <div class="text-center">Olá</div>
+      <div class="text-center"></div>
   </div>
 </template>
 
 <script>
 
-import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
+
 
 export default {
-  name: "app",
-
+    data(){
+      return {
+        usuarios: []
+      }
+    },
+    methods: {
+      obterUsuarios(){
+        this.usuarios = this.$http.get("/users").then(res => {
+          this.usuarios = res.data;
+          // eslint-disable-next-line no-console
+          console.log(this.usuarios);
+        });
+      }
+    },
+    created(){
+      // eslint-disable-next-line no-console
+      console.log("Carregou");
+      this.obterUsuarios();
+      }
 }
 </script>
 
